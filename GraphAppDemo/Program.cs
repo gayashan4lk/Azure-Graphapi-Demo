bffe66c36b04284f969c818db61aa60a41d93782
gayashan4lk <gayashan4lk@gmail.com>
2024-06-08 20:00:46 +0530

    first commit

---
+++ GraphAppDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Graph;
+using Azure.Identity;
+
+var client = new DefaultAzureCredential();
+
+var graphClient = new GraphServiceClient(client);
+
+try
+{
+    var usersPage = await graphClient.Users.GetAsync(reqConfig =>
+    {
+        reqConfig.QueryParameters.Orderby = new string []{ "DisplayName" };
+        reqConfig.QueryParameters.Top = 250;
+    });
+
+    if (usersPage?.Value == null)
+    {
+        Console.WriteLine("Returned null :(");
+        return;
+    }
+
+    Console.WriteLine($"Total user count: {usersPage.Value.Count}\n");
+
+    Console.WriteLine("ID | DisplayName | JobTitle | Created");
+
+    foreach (var user in usersPage.Value)
+    {
+        Console.WriteLine($"{user.Id} - {user.DisplayName} - {user.JobTitle} - {user.CreatedDateTime}");
+    }
+
+    Console.WriteLine("Executed successfully :)");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error occured. {ex.Message}");
+}
+
+
+
+
+
